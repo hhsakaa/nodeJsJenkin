b7c1381cbef228362cf14d617f0288d064ab2512
Ubuntu <ubuntu@ip-172-31-26-35.ec2.internal>
2024-09-04 08:48:02 +0000

    check

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
  
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello, World again and again and again!');
+  res.send('Hello, World!');
 });
  
 // Define the port number and start the server
