f8aaf72fdcb81a985923ee1a84efbbb8f8c3f1b4
Ubuntu <ubuntu@ip-172-31-26-35.ec2.internal>
2024-09-04 13:05:21 +0000

    pipelineTroubleshoot

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
  
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello, this is updayted response from pipeline!');
+  res.send('Hey There, this is updated response from pipeline!');
 });
  
 // Define the port number and start the server
